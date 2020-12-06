4d02f0d45ed64de335acc7cb9dd529db16226c4d
Jakub Kaczmarczyk <j.kaczmarczyk95@gmail.com>
2020-12-06 14:55:08 +0100

    Sudoku APP EMPTY

---
+++ src/main/java/SudokuTheGame.java
@@ -0,0 +1,16 @@
+public class SudokuTheGame {
+
+    public static void main(String[] args) throws InterruptedException {
+        Greeting greeting = new Greeting();
+        greeting.getGreeting();
+        // System.out.println("control: mistake checker = " + greeting.getMistakeChecker());
+
+        Difficulty difficulty = new Difficulty();
+        difficulty.getDifficulty();
+        // System.out.println("control: difficulty checker = " + difficulty.getDifficultyLevel());
+
+        Areas initialArea = new Areas();
+        initialArea.getEmptyArea();
+
+    }
+}
